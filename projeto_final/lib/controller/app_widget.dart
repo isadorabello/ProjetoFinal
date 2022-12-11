c0e933869669d0c7cc0d7f2969b890d2328ee8b7
isadorabello <isadora.bello@gec.inatel.br>
2022-12-11 19:18:15 -0300

    feat: adding new controller class

---
+++ projeto_final/lib/controller/app_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_final/view/tmb_page.dart';
+import '../view/imc_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CalculaSaúde',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: "/imc",
+      routes: {
+        "/imc": (context) => const IMCPage(),
+        "/tmb": (context) => const TMBPage(),
+      },
+    );
+  }
+}
